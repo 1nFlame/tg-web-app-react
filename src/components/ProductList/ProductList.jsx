16e3b7ff643198d90a6e74923b450f6ca0a26a4a
1nFlame <em-art@internet.ru>
2023-06-28 15:42:27 +0300

    init1

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,16 +4,16 @@ import ProductItem from "../ProductItem/ProductItem";
 import {useTelegram} from "../../hooks/useTelegram";
 
 const products = [
-    {id: '1', title: 'Штаны Square', price: 7200, description: 'Бордовый цвет. Size: S/M/L'},
-    {id: '2', title: 'Штаны Warfare', price: 11000, description: 'Черный цвет. Size: S/M/L'},
-    {id: '3', title: 'Штаны Cross', price: 8200, description: 'NAVY цвет. Size: S/M/L'},
-    {id: '4', title: 'Анорак Square', price: 8000, description: 'Зеленый Темный цвет. Size: S/M/L'},
-    {id: '5', title: 'Пончо Punch', price: 4000 , description: 'Черный цвет. Size: L'},
-    {id: '6', title: 'Анорак Mob', price: 3600 , description: 'Черный цвет. Size: M'},
-    {id: '7', title: 'Панама Bucket', price: 1900, description: 'Черный цвет. Size: S'},
-    {id: '8', title: 'Сумка-авоська', price: 1000 , description: 'Болотный цвет. One Size'},
-    {id: '9', title: 'Футболка Regular', price: 2700, description: 'Синий темный цвет. Size:XS/S/M'},
-    {id: '10', title: 'Шапка Incognito', price: 1300, description: 'Деним винтаж цвет. One Size'},
+    {id: '1', title: 'Штаны Square', price: 7200, description: 'Бордовый цвет.'},
+    {id: '2', title: 'Штаны Warfare', price: 11000, description: 'Черный цвет.'},
+    {id: '3', title: 'Штаны Cross', price: 8200, description: 'NAVY цвет.'},
+    {id: '4', title: 'Анорак Square', price: 8000, description: 'Зеленый цвет'},
+    {id: '5', title: 'Пончо Punch', price: 4000 , description: 'Черный цвет.'},
+    {id: '6', title: 'Анорак Mob', price: 3600 , description: 'Черный цвет.'},
+    {id: '7', title: 'Панама Bucket', price: 1900, description: 'Черный цвет.'},
+    {id: '8', title: 'Сумка-авоська', price: 1000 , description: 'Болотный цвет.'},
+    {id: '9', title: 'Футболка Regular', price: 2700, description: 'Синий цвет.'},
+    {id: '10', title: 'Шапка Incognito', price: 1300, description: 'Деним цвет.'},
 ]
 
 const getTotalPrice = (items) => {
