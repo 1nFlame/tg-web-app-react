2c8f789eb6ca08be05511862032573778125b345
1nFlame <em-art@internet.ru>
2023-06-28 15:44:21 +0300

    init1

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -12,10 +12,10 @@ const ProductItem = ({product, className, onAdd}) => {
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
             <div className={'price'}>
-                <span>Стоимость: <b>{product.price}</b></span>
+                <span>Цена: <b>{product.price}</b></span>
             </div>
             <Button className={'add-btn'} onClick={onAddHandler}>
-                Добавить в корзину>
+                В корзину
 
             </Button>
 
